86c56382e16be73b6b380b2aa5150ad87c4ee6af
emmmamuli <kilokoemma175@gmail.com>
2024-02-07 13:58:50 -0800

    class BaseModel

---
+++ models/base_model.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+from datetime import datetime
+import uuid
+import models
+"Defines class BaseModel"
+
+
+class BaseModel:
+    def __init__(self):
+        """initializes class BaseModel"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """string representation of the object"""
+        return "[{}] [{}] {}".format(self.__class__.__name__, self.id, self.__dict__)
+    
+    def save(self):
+        """updates currrent instance attribute with the current datetime"""
+        self.updated_at = datetime.now()
+    
+    def to_dict(self):
+        """Dictionary representation of the object"""
+        dict_obj = self.__dict__.copy()
+        dict_obj['__class__'] = self.__class__.__name__
+        dict_obj['created_at'] = self.created_at.isoformat()
+        dict_obj['updated_at'] = self.updated_at.isoformat()
+        return dict_obj
