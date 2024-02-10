f91f0d174da24dd637cade552b62558cc3b1740d
emmmamuli <kilokoemma175@gmail.com>
2024-02-09 17:56:38 -0800

    task5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,6 +11,10 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
+    def __init__(self):
+        """Creates instance of FIleStorage"""
+        pass
+
     def all(self):
         """Returns the dictionary __objects"""
         return self.__objects
@@ -25,11 +29,11 @@ class FileStorage:
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
         serialized_obj = {}
-        for key, value in FileStorage.__ojects.items():
+        for key, value in FileStorage.__objects.items():
             serialized_obj[key] = value.to_dict()
 
-        with open(FileStorage.__file_path, 'w', encoding="utf8") as my_file:
-            json.dump(serialized_obj, my_file)
+            with open(FileStorage.__file_path, 'w', encoding="utf8") as file:
+                json.dump(serialized_obj, file)
 
     def reload(self):
         """deserializes the JSON file to __objects
