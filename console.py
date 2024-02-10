bc85a68afc45c6f44fb0cec8c8c81968845d8080
emmmamuli <kilokoemma175@gmail.com>
2024-02-10 12:00:04 -0800

    correction

--- console.py
+++ console.py
@@ -29,70 +29,110 @@ class HBNBCommand(cmd.Cmd):
     def do_create(self, arg):
         """Creates a new instance of BaseModel and saves it to
         JSON file and prints the id"""
-        if not args:
-            print("** class name missing **")
+        args = inp.split()
+        if not self.class_verification(args):
             return
-        try:
-            new_instance = eval(arg)()
-            new_instance.save()
-            print(new_instance.id)
-        except NameError:
-            print("** class doesn't exist **")
+
+        inst = eval(str(args[0]) + '()')
+        if not isinstance(inst, BaseModel):
+            return
+        inst.save()
+        print(inst.id)
 
     def do_show(self, arg):
         """Prints the string representation of an instance based on
-        class name and id"""
-        args = arg.split()
-        if not arg:
+        class name and id """
+        args = inp.split()
+
+        if not self.class_verification(args):
+            return
+
+        if not self.id_verification(args):
+            return
+
+        string_key = str(args[0]) + '.' + str(args[1])
+        objects = models.storage.all()
+        print(objects[string_key])
+
+    def class_verification(cls, args):
+        """Verifies class and checks if it is in the class list.
+
+        Returns:
+            bool: True or false depending on status of class.
+        """
+        if len(args) == 0:
             print("** class name missing **")
-            return
-        if args[0] not in ["BaseModel"]:
+            return False
+
+        if args[0] not in cls.classes_list:
             print("** class doesn't exist **")
-            return
+            return False
+
+        return True
+
+    def id_verification(args):
+        """Verifies id of class.
+
+        Returns:
+            bool: True or False depending on status of id.
+        """
         if len(args) < 2:
             print("** instance id missing **")
-            return
-        key = args[0] + "." + args[1]
-        if key not in storage.all():
+            return False
+
+        objects = models.storage.all()
+        string_key = str(args[0]) + '.' + str(args[1])
+        if string_key not in objects.keys():
             print("** no instance found **")
-            return
-        del storage.all()[key]
-        storage.save()
+            return False
+
+        return True
 
     def do_all(self, arg):
         """Prints all string represntetion of all instances based
         or not on the class name"""
-        args = arg.split()
-        if args and args[0] not in ["BaseModel"]:
+        args = inp.split()
+        all_objects = models.storage.all()
+        list_ = []
+        if len(args) == 0:
+            # print all classes
+            for value in all_objects.values():
+                list_.append(str(value))
+        elif args[0] in self.classes_list:
+            # print just arg[0] class instances
+            for (key, value) in all_objects.items():
+                if args[0] in key:
+                    list_.append(str(value))
+        else:
             print("** class doesn't exist **")
-            return
-        print([str(obj) for obj in storage.all().values()])
+            return False
+        print(list_)
 
     def do_update(self, arg):
         """Updates an istance based on the class name and id"""
-        args = arg.split()
-        if not arg:
-            print("** class name missing **")
+        act = ""
+        for argv in line.split(','):
+            act = act + argv
+        args = shlex.split(act)
+        if not self.class_verification(args):
             return
-        if args[0] not in ["BaseModel"]:
-            print("** class doesn't exist **")
+        if not self.id_verification(args):
             return
-        if len(args) < 2:
-            print("** instance id missing **")
+        if not self.attribute_verification(args):
             return
-        key = args[0] + "." + args[1]
-        if key not in storage.all():
-            print("** no instance found **")
-            return
-        if len(args) < 3:
-            print("** attribute name missing **")
-            return
-        if len(args) < 4:
-            print("** value missing **")
-            return
-        obj = storage.all()[key]
-        setattr(obj, args[2], args[3])
-        storage.save()
+        all_objects = models.storage.all()
+        for key, value in all_objects.items():
+            object_name = value.__class__.__name__
+            object_id = value.id
+            if object_name == args[0] and object_id == args[1].strip('"'):
+                if len(args) == 2:
+                    print("** attribute name missing **")
+                elif len(args) == 3:
+                    print("** value missing **")
+                else:
+                    setattr(value, args[2], args[3])
+                    models.storage.save()
+                return
 
 
 if __name__ == '__main__':
