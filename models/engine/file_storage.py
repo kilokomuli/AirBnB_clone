f1266b33586cd7ce42ead08b8155f91a48fb3ad7
emmmamuli <kilokoemma175@gmail.com>
2024-02-11 09:58:41 -0800

    revert

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -32,11 +32,24 @@ class FileStorage:
         key = obj.__class__.__name__ + "." + obj.id
         self.__objects[key] = obj
 
+    def serialize_user(self, user):
+        """Serializes a User instance into dictionary format"""
+        user_dict = user.to_dict()
+        return user_dict
+
+    def deserialize_user(self, user_dict):
+        """Deserializes a dictionary into a User instance"""
+        user_instance = User(**user_dict)
+        return user_instance
+
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
         serialized_obj = {}
         for key, value in FileStorage.__objects.items():
-            serialized_obj[key] = value.to_dict()
+            if isinnstance(value, User):
+                serialized_obj[key] = self.serialize_user(value)
+            else:
+                serialized_obj[key] = value.to_dict()
 
         with open(FileStorage.__file_path, 'w', encoding="utf8") as file:
             json.dump(serialized_obj, file)
@@ -48,9 +61,15 @@ class FileStorage:
         try:
             with open(self.__file_path, 'r', encoding='utf-8') as myFile:
                 my_obj_dump = myFile.read()
-        except Exception:
+        except FileNotFoundError:
             return
-        objects = eval(my_obj_dump)
-        for (key, value) in objects.items():
-            objects[key] = eval(key.split('.')[0] + '(**value)')
+        objects = json.loads(my_obj_dump)
+        for key, value in objects.items():
+            class_name = value.get('__class__')
+            if class_name == 'User':
+                objects[key] = self.deserialize_user(value)
+            else:
+                module = getattr(models, class_name, None)
+                if module:
+                    objects[key] = module(**value)
         self.__objects = objects
