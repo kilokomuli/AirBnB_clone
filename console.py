50267903260d4b23a4a4556b95cf2f51634924dd
emmmamuli <kilokoemma175@gmail.com>
2024-02-11 07:06:38 -0800

    classes

--- console.py
+++ console.py
@@ -10,12 +10,18 @@ from models.base_model import BaseModel
 from models.__init__ import storage
 import models
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
     """ class cmd"""
     prompt = "(hbnb) "
-    classes_list = ["BaseModel", "User"]
+    classes_list = ["BaseModel", "User". "Place", "State", "City",
+            "Amenity", "Review"]
     commands_list = ["create", "show", "all", "destroy", "update", "count"]
 
     def do_quit(self, arg):
